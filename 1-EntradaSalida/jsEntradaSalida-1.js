cc5c66cd1275c301f98f492e55ba6abc48ebafd6
alumno <alumno>
2017-02-06 12:22:19 -0300

    Tarea realizada

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,9 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
-{
-	
+{   
+//alert ("It's a trap!!!");	
+//confirm ("Atada")
+//prompt("Corsaira")
+var nombre= prompt ("Ingrese su nombre")
 }
 
