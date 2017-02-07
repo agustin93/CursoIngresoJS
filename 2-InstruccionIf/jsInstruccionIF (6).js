52bed347979c39e0f8dd6c7356c460967b839cf1
alumno <alumno>
2017-02-07 12:29:05 -0300

    Exito

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad=document.getElementById("edad").value;
+if(edad>=18){
+    alert ("Usted es mayor de edad")
+}
+else{
+    if(edad>=13){
+        alert ("Usted es adolescente")
+    }
+    else
+    {
+           alert ("Usted es un niño")
+    }
+}
 
 
+    
 
 
 }//FIN DE LA FUNCIÓN
