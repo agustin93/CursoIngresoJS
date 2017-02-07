a46e06e42aa6aadcae78f193026ff3c3d1a32d3c
alumno <alumno>
2017-02-07 12:08:23 -0300

    resolviending

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,10 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
-	
+	var num1=document.getElementById("sueldo").value;
+    num1=parseInt(num1);
+    resultado=(num1*1.1);
+    var resultado;
+    document.getElementById("resultado")=resultado;
+    prompt("Resultado")=resultado;
 }
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,8 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
+var edad=document.getElementById("edad").value;
+if(edad>17 || edad<13){
+alert ("Usted no es adolescente");
+}
 }//FIN DE LA FUNCIÓN
